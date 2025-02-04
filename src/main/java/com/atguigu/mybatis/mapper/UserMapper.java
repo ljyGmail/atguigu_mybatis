9eb7dc9751c2d479f0226f88ef9642137c0ecc8e
liangjinyong <liangjy881@gmail.com>
2025-02-04 13:19:51 +0900

    10 搭建MyBatis框架之创建mapper的映射文件

--- src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -1,4 +1,16 @@
 package com.atguigu.mybatis.mapper;
 
 public interface UserMapper {
+    /**
+     * MyBatis面向接口编程的两个一致:
+     * 1.映射文件的namespace要和mapper接口的全类名保持一致
+     * 2.映射文件中SQL语句的id要和mapper接口的方法名一致
+     *
+     * 表 -- 实体类 -- mapper接口 -- 映射文件
+     */
+
+    /**
+     * 添加用户信息
+     */
+    int insertUser();
 }
