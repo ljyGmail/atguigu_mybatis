12e5b7503440674d2611348b72ee132a3f75fa8a
liangjinyong <liangjy881@gmail.com>
2025-02-05 00:32:21 +0900

    35,36,37 Mybatis 处理批量删除，动态设置表名，获取添加功能自增的主键

--- src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
+++ src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
@@ -11,4 +11,19 @@ public interface SQLMapper {
      * 根据用户名模糊查询用户信息
      */
     List<User> getUserByLike(@Param("username") String username);
+
+    /**
+     * 批量删除
+     */
+    int deleteMultipleUsers(@Param("ids") String ids);
+
+    /**
+     * 查询指定表中的数据
+     */
+    List<User> getUserByTableName(@Param("tableName") String tableName);
+
+    /**
+     * 添加用户信息
+     */
+    void insertUser(User user);
 }
--- src/test/java/com/atguigu/mybatis/test/D_SQLMapperTest.java
+++ src/test/java/com/atguigu/mybatis/test/D_SQLMapperTest.java
@@ -17,4 +17,40 @@ public class D_SQLMapperTest {
         List<User> list = mapper.getUserByLike("a");
         System.out.println(list);
     }
+
+    @Test
+    public void testDeleteMultipleUsers() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        int result = mapper.deleteMultipleUsers("1,2,3");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetUserByTableName() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> list = mapper.getUserByTableName("t_user");
+        System.out.println(list);
+    }
+
+    /*
+    @Test
+    public void testJDBC() throws Exception {
+        Class.forName("");
+        Connection connection = DriverManager.getConnection("", "", "");
+        PreparedStatement ps = connection.prepareStatement("insert", Statement.RETURN_GENERATED_KEYS);
+        ps.executeUpdate();
+        ResultSet resultSet = ps.getGeneratedKeys();
+    }
+     */
+
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        User user = new User(null, "王五", "123", 23, "男", "123@163.com");
+        mapper.insertUser(user);
+        System.out.println(user);
+    }
 }
