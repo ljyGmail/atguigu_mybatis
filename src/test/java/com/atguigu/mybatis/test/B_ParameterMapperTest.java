cc0a7cd1b9c6bf25997d441c4fe93e5344009324
liangjinyong <liangjy881@gmail.com>
2025-02-04 18:11:57 +0900

    23 Mybatis 获取参数值的两种方式#{}和

--- src/test/java/com/atguigu/mybatis/test/B_ParameterMapperTest.java
+++ src/test/java/com/atguigu/mybatis/test/B_ParameterMapperTest.java
@@ -10,6 +10,12 @@ import java.util.List;
 
 public class B_ParameterMapperTest {
 
+    /**
+     * MyBatis获取参数值的两种方式: ${}和#{}
+     * ${}: 本质是字符串拼接
+     * #{}: 本质是占位符赋值
+     */
+
     @Test
     public void getAllUsers() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
@@ -17,4 +23,16 @@ public class B_ParameterMapperTest {
         List<User> list = mapper.getAllUsers();
         list.forEach(System.out::println);
     }
+
+    /*
+    @Test
+    public void testJDBC() throws Exception {
+        String username = "admin";
+        Class.forName("");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+        // PreparedStatement ps = connection.prepareStatement("select * from t_user where username = '" + username + "'");
+        PreparedStatement ps = connection.prepareStatement("select * from t_user wher username = ?");
+        ps.setString(1, username);
+    }
+    */
 }
