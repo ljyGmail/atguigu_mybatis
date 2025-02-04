954698d9ff6ff5f5a453ad11e494f2cb780fc635
liangjinyong <liangjy881@gmail.com>
2025-02-04 15:24:39 +0900

    13,14 搭建MyBatis框架之修改，删除和查询功能

--- src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -1,5 +1,9 @@
 package com.atguigu.mybatis.mapper;
 
+import com.atguigu.mybatis.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     /**
      * MyBatis面向接口编程的两个一致:
@@ -13,4 +17,24 @@ public interface UserMapper {
      * 添加用户信息
      */
     int insertUser();
+
+    /**
+     * 修改用户信息
+     */
+    void updateUser();
+
+    /**
+     * 删除用户信息
+     */
+    void deleteUser();
+
+    /**
+     * 根据id查询用户信息
+     */
+    User getUserById();
+
+    /**
+     * 查询所有的用户信息
+     */
+    List<User> getAllUsers();
 }
--- src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
+++ src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
@@ -1,6 +1,7 @@
 package com.atguigu.mybatis.test;
 
 import com.atguigu.mybatis.mapper.UserMapper;
+import com.atguigu.mybatis.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MyBatisTest {
 
@@ -34,5 +36,29 @@ public class MyBatisTest {
         // 提交事务
         // sqlSession.commit();
         System.out.println("result:" + result);
+    }
+
+    @Test
+    public void testCRUD() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        // mapper.updateUser();
+        // mapper.deleteUser();
+        /*
+        User user = mapper.getUserById();
+        System.out.println(user);
+         */
+        List<User> list = mapper.getAllUsers();
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * 删除用户信息
+     */
+    @Test
+    public void deleteUser() throws IOException {
+
     }
 }
