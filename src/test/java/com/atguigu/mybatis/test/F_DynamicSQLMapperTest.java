c61cb1268adf388d8dc2bab599fa6d20a89516d5
liangjinyong <liangjy881@gmail.com>
2025-02-05 13:21:44 +0900

    51 MyBatis 动态SQL之trim标签

--- src/test/java/com/atguigu/mybatis/test/F_DynamicSQLMapperTest.java
+++ src/test/java/com/atguigu/mybatis/test/F_DynamicSQLMapperTest.java
@@ -16,13 +16,18 @@ public class F_DynamicSQLMapperTest {
      * 2. where: 当where标签中有内容时，会自动生成where关键字，并且将内容前多余的and或or去掉
      * 当where标签中没有内容时，此时where没有任何效果
      * 注意: where不能将其中内容后面多余的and或or去掉
+     * 3. trim:
+     * 若标签中有内容时:
+     * prefix|suffix: 将trim标签中内容前面或后面添加指定内容
+     * suffixOverrides|prefixOverrides: 将trim标签中内容前面或后面去掉指定内容
+     * 若标签中没有内容时，trim标签也没有任何效果
      */
 
     @Test
     public void testGetEmpsByCondition() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
-        List<Emp> list = mapper.getEmpsByCondition(new Emp(null, "张三", 23, "", null));
+        List<Emp> list = mapper.getEmpsByCondition(new Emp(null, "", null, "", null));
         System.out.println(list);
     }
 }
