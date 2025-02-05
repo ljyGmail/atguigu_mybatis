bb86460c41f6270d2e82e2011f799f2afcc57e32
liangjinyong <liangjy881@gmail.com>
2025-02-05 13:13:46 +0900

    50 MyBatis 动态SQL之where标签

--- src/test/java/com/atguigu/mybatis/test/F_DynamicSQLMapperTest.java
+++ src/test/java/com/atguigu/mybatis/test/F_DynamicSQLMapperTest.java
@@ -13,13 +13,16 @@ public class F_DynamicSQLMapperTest {
     /**
      * 动态SQL:
      * 1. if: 根据标签中test属性所对应的表达式决定标签中的内容是否需要拼接到SQL中
+     * 2. where: 当where标签中有内容时，会自动生成where关键字，并且将内容前多余的and或or去掉
+     * 当where标签中没有内容时，此时where没有任何效果
+     * 注意: where不能将其中内容后面多余的and或or去掉
      */
 
     @Test
     public void testGetEmpsByCondition() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
-        List<Emp> list = mapper.getEmpsByCondition(new Emp(null, "张三", 23, "男", "123@qq.com"));
+        List<Emp> list = mapper.getEmpsByCondition(new Emp(null, "张三", 23, "", null));
         System.out.println(list);
     }
 }
